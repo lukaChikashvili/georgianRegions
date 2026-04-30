160b2b55429f46e67a98337ff0f80e430919acf2
luka chikashvili <lukachikashvili2020@gmail.com>
2026-04-30 19:44:55 +0400

    add header design

--- Desktop/projects/gym/gympulse/components/Header.jsx
+++ Desktop/projects/gym/gympulse/components/Header.jsx
@@ -1,11 +1,19 @@
 "use client"
-import { SignInButton, UserButton } from '@clerk/nextjs';
+import { SignInButton, UserButton, useUser } from '@clerk/nextjs';
 import { useConvexAuth } from 'convex/react';
+import Image from 'next/image';
+import logo from '../public/logo.png'
+import { User } from 'lucide-react';
 
 
 const Header = () => {
 
     const {isAuthenticated, isLoading } = useConvexAuth();
+
+    const { user, isLoaded } = useUser();
+
+    if (!isLoaded) return <div>Loading...</div>;
+
 
     const links = [
         {id: 1, link: "/learn", title: "მთავარი"},
@@ -19,14 +27,19 @@ const Header = () => {
   return (
     <div className='w-full flex items-center justify-between px-12 py-4 border-b border-gray-300'>
     <div >
-        logo
+        <Image width = {170}  src = {logo} alt ="logo" />
     </div>
-    <div className='flex items-center gap-12'>
-       {links.map((value) => (
-        <div key={value.id}>
-            <h2>{value.title}</h2>
-        </div>
-       ))}
+    <div className='flex items-center gap-8'>
+    {links.map((value) => (
+  <div key={value.id} className="relative group cursor-pointer">
+    <h2 className="font-semibold text-gray-600 transition-colors duration-300 group-hover:text-black">
+      {value.title}
+    </h2>
+
+   
+    <span className="absolute left-0 -bottom-1 w-0 h-[0.5] bg-blue-600 transition-all duration-300 group-hover:w-full"></span>
+  </div>
+))}
    </div>
 
     <div className='flex items-center gap-6'>
@@ -36,7 +49,15 @@ const Header = () => {
        {!isAuthenticated && !isLoading && (
           <>
             <SignInButton mode = "modal">
-               <button className='Btn'>Start Free</button>
+            <button className="group relative px-6 py-2 rounded-xl font-medium text-white 
+  bg-linear-to-r from-blue-500 to-purple-600 
+  hover:from-blue-600 hover:to-purple-700 
+  shadow-lg hover:shadow-xl transition-all duration-300
+  flex items-center gap-3 cursor-pointer">
+    
+    <User className="w-4 h-4 group-hover:scale-110 transition" />
+    რეგისტრაცია
+  </button>
             </SignInButton>
 
             
@@ -44,7 +65,11 @@ const Header = () => {
        )}
   
        {isAuthenticated && !isLoading && (
-          <UserButton afterSwitchSessionUrl='/'/>
+          <>
+           <h1 className='text-[#008BFF] font-bold'>გამარჯობა, {user?.firstName}</h1>
+           <UserButton afterSwitchSessionUrl='/'/>
+          </>
+         
        )}
     </div>
 
