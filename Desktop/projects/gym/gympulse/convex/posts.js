a5b6f980e30e8f2738490b3c34a45f74bebe7524
luka chikashvili <lukachikashvili2020@gmail.com>
2026-04-30 20:18:21 +0400

    add post creation schema

---
+++ Desktop/projects/gym/gympulse/convex/posts.js
@@ -0,0 +1,18 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+
+
+export const createPost = mutation({
+    args: {title: v.string(), body: v.string()},
+    handler: async (ctx, args) => {
+        const newBlog = await ctx.db.insert("posts", {
+            body: args.body,
+            title: args.title,
+            authorId: "sdd"
+        });
+
+        return newBlog;
+    }
+});
+
