c75ae75084661b0ff7ac9d9964f14a081a3b7633
luka chikashvili <lukachikashvili2020@gmail.com>
2026-05-17 16:46:24 +0400

    memorial convex table

--- Desktop/projects/gym/gympulse/convex/schema.js
+++ Desktop/projects/gym/gympulse/convex/schema.js
@@ -17,5 +17,31 @@ export default defineSchema({
          body: v.string(),
          authorId: v.string(),
          authorName: v.string(),
-    })
+    }),
+
+
+    memorials: defineTable({
+        firstName: v.string(),
+        lastName: v.string(),
+        birthDate: v.string(),
+        deathDate: v.string(),
+        location: v.string(),
+        mainPortraitUrl: v.optional(v.string()),
+
+        epitaph: v.string(),
+        biography: v.string(),
+
+        musicTrack: v.string(),     
+        gravestoneModel: v.string(),  
+        enableCandle: v.boolean(),    
+        candleCount: v.number(),
+
+        urlSlug: v.string(),
+        privacyType: v.string(),      
+        requireModeration: v.boolean(),
+
+        creatorId: v.string(),       
+        creatorName: v.string(),
+    }).index("by_urlSlug", ["urlSlug"]) 
+    .index("by_creatorId", ["creatorId"]),
 });
