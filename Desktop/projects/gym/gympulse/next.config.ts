73aa438f8141e003293d0753c7437f3c57821534
luka chikashvili <lukachikashvili2020@gmail.com>
2026-05-20 19:45:46 +0400

    update next config

--- Desktop/projects/gym/gympulse/next.config.ts
+++ Desktop/projects/gym/gympulse/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: [
+    "@clerk/nextjs", 
+    "@clerk/shared",
+    "three",
+    "@react-three/fiber",
+    "@react-three/drei"
+  ],
 };
 
 export default nextConfig;
