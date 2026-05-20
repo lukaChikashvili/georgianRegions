9fc54fe8ecfcd9da2df3894841a083193fd3a03b
luka chikashvili <lukachikashvili2020@gmail.com>
2026-05-20 20:15:02 +0400

    add middleware update

--- Desktop/projects/gym/gympulse/middleware.js
+++ Desktop/projects/gym/gympulse/middleware.js
@@ -1,11 +1,11 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-
-export const runtime = "nodejs"; 
-
 export default clerkMiddleware();
 
 export const config = {
+ 
+  runtime: "nodejs", 
+  
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
