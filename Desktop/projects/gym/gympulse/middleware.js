4f24b94d1607f86c91b6c71b079f75963ed282e3
luka chikashvili <lukachikashvili2020@gmail.com>
2026-06-14 20:54:37 +0400

    update middleware glb file

--- Desktop/projects/gym/gympulse/middleware.js
+++ Desktop/projects/gym/gympulse/middleware.js
@@ -9,6 +9,8 @@ const isPublicRoute = createRouteMatcher([
   "/grave",
   "/grave/(.*)",
   "/services",
+  "/blog",
+  "/blog/(.*)",
   "/services/(.*)",
   "/robots.txt",
   "/sitemap.xml",
@@ -23,7 +25,7 @@ export default clerkMiddleware(async (auth, request) => {
 export const config = {
   runtime: "nodejs",
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|glb|gltf|bin|hdr|exr|mp3|mp4|wasm)).*)',
     '/(api|trpc)(.*)',
   ],
 };
